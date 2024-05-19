2208ad3fcf0a010b0484aab2f3e717d945545b2b
Harsh Raj <saxenahharsh73@gmail.com>
2024-05-19 19:27:20 +0530

    final

---
+++ src/pages/Model.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from '../components/Content';
+import ImageScreen from '../components/Image';
+
+const Model = () => {
+  return (
+    <>
+    <ImageScreen />
+    <Content />
+    </>
+  )
+}
+
+export default Model
